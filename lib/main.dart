f53df895f0d5561f251b2b711e4031662b777189
mohamed_Elsayed <mh3010357@gmail.com>
2025-07-15 13:39:39 +0300

    initialize app and implement native splash screen

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:evently/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(EventlyApp());
+}
+
+class EventlyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {HomeScreen.routName: (_) => HomeScreen()},
+    );
+  }
+}
