e01c2b6c34e1d97d528c503e28b0b2f4067fad99
mohamed_Elsayed <mh3010357@gmail.com>
2025-07-19 17:04:05 +0300

    create home header

---
+++ lib/tabs/home/home_header.dart
@@ -0,0 +1,79 @@
+import 'package:evently/app_theme.dart';
+import 'package:evently/models/category_model.dart';
+import 'package:evently/tabs/home/tab_item.dart';
+import 'package:flutter/material.dart';
+
+class HomeHeader extends StatefulWidget {
+  const HomeHeader({super.key});
+
+  @override
+  State<HomeHeader> createState() => _HomeHeaderState();
+}
+
+class _HomeHeaderState extends State<HomeHeader> {
+  int index = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      padding: EdgeInsets.only(left: 16, bottom: 16),
+      decoration: BoxDecoration(
+        color: AppTheme.primary,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(24),
+          bottomRight: Radius.circular(24),
+        ),
+      ),
+      child: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Welcome Back ✨', style: textTheme.titleSmall),
+            Text('User Name', style: textTheme.headlineSmall),
+            SizedBox(height: 16),
+            DefaultTabController(
+              length: CategoryModel.categories.length + 1,
+              child: TabBar(
+                isScrollable: true,
+                dividerColor: Colors.transparent,
+                indicatorColor: Colors.transparent,
+                tabAlignment: TabAlignment.start,
+                labelPadding: EdgeInsets.only(right: 10),
+                onTap: (value) {
+                  if (value != index) {
+                    index = value;
+                    setState(() {});
+                  }
+                },
+                tabs: [
+                  TabItem(
+                    label: 'All',
+                    icon: Icons.ac_unit_outlined,
+                    isSelected: index == 0,
+                    selectedForegroundColor: AppTheme.primary,
+                    unSelectedForegroundColor: AppTheme.white,
+                    selectedBackgroundColor: AppTheme.white,
+                  ),
+                  ...CategoryModel.categories.map(
+                    (category) => TabItem(
+                      icon: category.icon,
+                      isSelected:
+                          CategoryModel.categories.indexOf(category) + 1 ==
+                          index,
+                      label: category.name,
+                      selectedForegroundColor: AppTheme.primary,
+                      unSelectedForegroundColor: AppTheme.white,
+                      selectedBackgroundColor: AppTheme.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/tabs/home/tab_item.dart
@@ -0,0 +1,52 @@
+import 'package:evently/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class TabItem extends StatelessWidget {
+  String label;
+  IconData icon;
+  bool isSelected;
+  Color selectedForegroundColor;
+  Color unSelectedForegroundColor;
+  Color selectedBackgroundColor;
+
+  TabItem({
+    required this.label,
+    required this.icon,
+    required this.isSelected,
+    required this.selectedForegroundColor,
+    required this.unSelectedForegroundColor,
+    required this.selectedBackgroundColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      decoration: BoxDecoration(
+        color: isSelected ? selectedBackgroundColor : Colors.transparent,
+        borderRadius: BorderRadius.circular(46),
+        border: isSelected ? null : Border.all(color: AppTheme.white),
+      ),
+      child: Row(
+        children: [
+          Icon(
+            icon,
+            size: 24,
+            color: isSelected
+                ? selectedForegroundColor
+                : unSelectedForegroundColor,
+          ),
+          SizedBox(width: 8),
+          Text(
+            label,
+            style: Theme.of(context).textTheme.titleMedium!.copyWith(
+              color: isSelected
+                  ? selectedForegroundColor
+                  : unSelectedForegroundColor,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
