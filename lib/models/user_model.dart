b0900973f77194fe98a199541605f1e0aad8ae18
mohamed_Elsayed <mh3010357@gmail.com>
2025-08-04 12:20:49 +0300

    add firebase functions for favorite

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -2,11 +2,27 @@ class UserModel {
   String id;
   String name;
   String email;
+  List<String> favoriteEventIds;
 
-  UserModel({required this.id, required this.name, required this.email});
+  UserModel({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.favoriteEventIds,
+  });
 
   UserModel.fromJson(Map<String, dynamic> json)
-    : this(id: json['id'], name: json['name'], email: json['email']);
+    : this(
+        id: json['id'],
+        name: json['name'],
+        email: json['email'],
+        favoriteEventIds: json['favoriteEventIds'],
+      );
 
-  Map<String, dynamic> toJson() => {'id': id, 'name': name, 'email': email};
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'name': name,
+    'email': email,
+    'favoriteEventIds': favoriteEventIds,
+  };
 }
